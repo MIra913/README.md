407c1a15efd3a5395919f6d09d94e9bd1bfc7406
MIra913 <pjanaki913@gmail.com>
2025-12-31 16:50:28 +0530

    Update AboutUs.jsx

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+about_us_content = """import React from 'react';
 
-function AboutUs() {
-  return (
-    <div>
-      <h1>About Our Company</h1>
-      <p>
-        We are a software development company focused on building high-quality
-        web applications and providing reliable digital solutions.
-      </p>
-    </div>
-  );
-}
+const AboutUs = () => {
+    return (
+        <div className="about-us-container">
+            <h2>About e-plantShopping</h2>
+            <p>We are dedicated to providing a wide variety of plants for your home and garden.</p>
+            <p>Our mission is to make plant shopping easy, fun, and sustainable.</p>
+        </div>
+    );
+};
 
 export default AboutUs;
+"""
+with open(f"{project_name}/AboutUs.jsx", "w") as f:
+    f.write(about_us_content)
