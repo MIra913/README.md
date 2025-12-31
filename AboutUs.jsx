2ee0c92245f6d70dbca0780dcf0d6410c7e17611
MIra913 <pjanaki913@gmail.com>
2025-12-31 15:39:51 +0530

    Add files via upload

---
+++ AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Our Company</h1>
+      <p>
+        We are a software development company focused on building high-quality
+        web applications and providing reliable digital solutions.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
